6ca53fb4ad49ead4fedc47fcad1e529bd22deedb
Andrea Biancini <andrea.biancini@gmail.com>
2015-05-23 08:45:20 +0200

    Fixed sonar

--- sonar.bat
+++ sonar.bat
@@ -5,7 +5,7 @@ IF EXIST TestResults RMDIR TestResults /S /Q
 SET VS_PATH=C:\Program Files (x86)\Microsoft Visual Studio 12.0
 SET TEST_DLL=UnitTests\bin\Debug
 
-rem "%VS_PATH%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "%TEST_DLL%\UnitTests.dll" "%TEST_DLL%\ApprovalUtilities.dll" "%TEST_DLL%\ApprovalUtilities.Net45.dll" "%TEST_DLL%\GildedRose.exe" /logger:trx /UseVsixExtensions:true /EnableCodeCoverage
+"%VS_PATH%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "%TEST_DLL%\UnitTests.dll" "%TEST_DLL%\ApprovalUtilities.dll" "%TEST_DLL%\ApprovalUtilities.Net45.dll" "%TEST_DLL%\GildedRose.exe" /logger:trx
 "%VS_PATH%\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" collect /output:VisualStudio.coverage "%VS_PATH%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "%TEST_DLL%\UnitTests.dll" "%TEST_DLL%\ApprovalUtilities.dll" "%TEST_DLL%\ApprovalUtilities.Net45.dll" "%TEST_DLL%\GildedRose.exe"
 "%VS_PATH%\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" analyze /output:VisualStudio.coveragexml VisualStudio.coverage
 sonar-runner.bat
