6f9dc742c83a7565cfd5a22e9aee8f85a1a70ff3
Andrea Biancini <andrea.biancini@gmail.com>
2017-04-30 10:19:46 +0200

    Fixed problems with sonar analyzer

--- sonar.bat
+++ sonar.bat
@@ -2,9 +2,16 @@ IF EXIST VisualStudio.coverage DEL VisualStudio.coverage
 IF EXIST VisualStudio.coveragexml DEL VisualStudio.coveragexml
 IF EXIST TestResults RMDIR TestResults /S /Q
 
-SET VS_PATH=C:\Program Files (x86)\Microsoft Visual Studio 12.0
-SET TEST_DLL=UnitTests\bin\Debug
+SET NET_PATH=C:\Program Files (x86)\MSBuild\14.0\Bin
+SET VS_PATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0
+SET NUNIT_PATH="%CD%\packages\NUnit.ConsoleRunner.3.6.1\tools"
+SET COVER_PATH="%CD%\packages\OpenCover.4.6.519\tools"
+SET TEST_DLL=UnitTests\bin\Debug\UnitTests.dll
+SET SONAR_PATH=C:\SonarQube
 
-"%VS_PATH%\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" collect /output:VisualStudio.coverage "%VS_PATH%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "%TEST_DLL%\UnitTests.dll" "%TEST_DLL%\ApprovalUtilities.dll" "%TEST_DLL%\ApprovalUtilities.Net45.dll" "%TEST_DLL%\GildedRose.exe"  /logger:trx
-"%VS_PATH%\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" analyze /output:VisualStudio.coveragexml VisualStudio.coverage
-sonar-runner.bat
+"%SONAR_PATH%\MSBuild.SonarQube.Runner.exe" begin /key:"it.reti.futureti" /name:"Gilded Rose Kata for CSharp" /version:"1.0" /d:sonar.language="cs" /d:sonar.sources="." /d:sonar.cs.nunit.reportsPaths="%CD%\TestResult.xml" /d:sonar.cs.opencover.reportsPaths="%CD%\TestCoverage.xml"
+
+"%NET_PATH%\MSBuild.exe" /t:Rebuild
+"%COVER_PATH%\OpenCover.Console.exe" -target:"%NUNIT_PATH%\nunit3-console.exe" -targetargs:"%TEST_DLL% --result=TestResult.xml;format=nunit2" -output:"TestCoverage.xml" -register:user
+
+"%SONAR_PATH%\MSBuild.SonarQube.Runner.exe" end
