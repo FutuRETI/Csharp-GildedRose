8a0bd772ace9a3e4999fbd2dceec0ab1345dbed0
Andrea Biancini <andrea.biancini@gmail.com>
2017-05-02 10:19:34 +0200

    Refactored text test fixture

--- GildedRose/TextTestFixture.cs
+++ GildedRose/TextTestFixture.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace GildedRose
 {
-    public class Program
+    public static class Program
     {
         private static IList<Item> GeneraItems()
         {
